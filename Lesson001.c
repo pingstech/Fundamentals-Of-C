d8a6dd22dd2dff52e878b721e520989a8404acbb
Furkan YAYLA <yaylafurkan41@gmail.com>
2021-11-26 23:40:22 +0300

    init

---
+++ Lesson001.c
@@ -0,0 +1,21 @@
+/*
+ * @file Lesson001.c
+ * @author Furkan YAYLA (yaylafurkan41@gmail.com)
+ * @version 0.1
+ * @date 2021-11-26
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Variables of type char occupy %u bytes \n",sizeof(char));
+    printf("Variables of type short occupy %u bytes \n",sizeof(short));
+    printf("Variables of type int occupy %u bytes \n",sizeof(int));
+    printf("Variables of type long occupy %u bytes \n",sizeof(long));
+    printf("Variables of type long long occupy %u bytes \n",sizeof(long long));
+    printf("Variables of type float occupy %u bytes \n",sizeof(float));
+    printf("Variables of type double occupy %u bytes \n",sizeof(double));
+    printf("Variables of type long double occupy %u bytes \n",sizeof(long double));
+
+    return 0;
+}
