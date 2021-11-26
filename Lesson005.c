643cb89716f228c6901a07c80066d3c3bafaecc8
Furkan YAYLA <yaylafurkan41@gmail.com>
2021-11-27 00:15:50 +0300

    Added Lesson005

---
+++ Lesson005.c
@@ -0,0 +1,24 @@
+/*
+ * @file:       Lesson001.c
+ * @author:     Furkan YAYLA (yaylafurkan41@gmail.com)
+ * @purpose:    Infinite loop and break statement
+ * @date:       2021-11-26
+ */
+#include<stdio.h>
+
+int main()
+{
+    int number;
+    while(1)
+    {
+        printf("Please enter a value: ");
+        scanf("%d",&number);
+
+        if(number < 0)
+            break;
+        else
+            printf("Square of %d is: %.3lf\n",number,(number*0.5));
+            break;
+    }
+    return 0;
+}
