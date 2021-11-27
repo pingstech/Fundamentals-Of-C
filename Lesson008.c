9d1421658d9f2288cfc12c103a48a655b69c9205
Furkan YAYLA <yaylafurkan41@gmail.com>
2021-11-27 19:03:48 +0300

    Do while loop example

---
+++ Lesson008.c
@@ -0,0 +1,25 @@
+/*
+ * @file:       Lesson007.c
+ * @author:     Furkan YAYLA (yaylafurkan41@gmail.com)
+ * @purpose:    Do while loop example
+ * @date:       2021-11-27
+ */
+
+#include<stdio.h>
+
+int main()
+{   
+    int num,sum=0;
+
+            printf("When you want to see the summary of the numbers please press 0\n");
+    do
+    {
+        printf("Please enter number for add: ");
+        scanf("%d",&num);
+        sum+=num;
+    } while (num!=0);
+    
+    printf("Sum of the numbers: %2.0d",sum);
+
+    return 0;
+}
