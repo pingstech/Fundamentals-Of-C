8e99fdab1fb5a9cbdb7db1c464e323e5dbf3671e
Furkan YAYLA <yaylafurkan41@gmail.com>
2021-11-29 21:44:48 +0300

    Array operation

---
+++ Lesson012.c
@@ -0,0 +1,24 @@
+/*
+ * @file:       Lesson011.c
+ * @author:     Furkan YAYLA (yaylafurkan41@gmail.com)
+ * @purpose:    Array operation
+ * @date:       2021-11-29
+ */
+
+#define MONTHS 12
+
+#include<stdio.h>
+
+int main(void)
+{   
+    int days[MONTHS]={31,28,31,30,31,30,31,31,30,31,30,31};         //Storing numerical value in integer array
+    char month[][MONTHS]={"January","February","March","April","May","June","July","August","September","October","November","December"};       //Storing string in char array
+    int index;
+
+    for(index=0;index<MONTHS;index++)
+    {
+        printf(" %10s has %2d days.\n",month[index],days[index]);
+    }
+
+    return 0;
+}
