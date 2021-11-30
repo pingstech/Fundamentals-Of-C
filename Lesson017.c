da8ff43f375660fe1cdbaf3f8e1fec808115416c
Furkan YAYLA <yaylafurkan41@gmail.com>
2021-11-30 20:12:32 +0300

    Creating prime numbers(with Array operations)

---
+++ Lesson017.c
@@ -0,0 +1,46 @@
+/*
+ * @file:       Lesson011.c
+ * @author:     Furkan YAYLA (yaylafurkan41@gmail.com)
+ * @purpose:    Creating prime numbers
+ * @date:       2021-11-30
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+int main()
+{ 
+    int indexP,indexI,primeIndex=2;
+    int primes[50]={};
+    bool isPrime;
+
+    //Hardcore prime numbers
+    primes[0]=2;
+    primes[1]=3;
+
+    for(indexP=5;indexP<=100;indexP++)
+    {
+        isPrime=true;
+        for (indexI=2;isPrime && indexI<indexP;indexI++)
+        {
+            if(indexP%indexI==0)
+            {
+                isPrime=false;
+            }
+        }
+        if(isPrime==true)
+        {
+            primes[primeIndex]=indexP;
+            primeIndex++;
+        }
+    }
+
+    //Printing prime numbers on screen
+    for(indexI=0;indexI<primeIndex;indexI++)
+    {
+        printf("%d ",primes[indexI]);
+    }
+    return 0;
+}
+
